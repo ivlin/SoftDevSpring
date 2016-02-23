4149bfb1d48044f775215f953f095ef0d0412b4a
Ivan Lin <ivlin112@gmail.com>
2016-02-23 09:59:59 -0500

    dvd logo

--- hw02/animate.js
+++ hw02/animate.js
@@ -1,13 +1,20 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
+ctx.strokeStyle = "#000000";
 ctx.fillStyle = "#FF00FF";
+ctx.beginPath();
+ctx.strokeRect(0,0,canvas.width,canvas.height);
+ctx.stroke();
+ctx.closePath();
 
-var animate=false;
+var requestId;
+var logo = new Image();
+logo.src = "logo_dvd.jpg";
 var radius=0;
 var isGrowing=false;
 
-var drawDot = function(){
+var drawDot = function drawDot(){
     ctx.beginPath();
     if (radius >= canvas.width/2 || radius <= 0){
 	isGrowing = !isGrowing;
@@ -17,16 +24,18 @@ var drawDot = function(){
     ctx.arc(canvas.width/2,canvas.height/2,radius,0,2*Math.PI);
     ctx.fill();
     ctx.closePath();
-    if (animate){
-	window.requestAnimationFrame(drawDot);
-    }
+    requestId = window.requestAnimationFrame(drawDot);
 };
 
-document.getElementById("start").addEventListener("click",function(){
-    animate=true;
-    window.requestAnimationFrame(drawDot);
-});
+var xpos,ypos,dir;
+var dvd = function dvd(){
+    ctx.drawImage(logo,dvdx,dvdy,canvas.width/6,logo.height*canvas.width/logo.width/6);
+};
 
-document.getElementById("stop").addEventListener("click",function(){
-    animate=false;
-});
+var stop = function stop(){
+    window.cancelAnimationFrame(requestId);
+};
+
+document.getElementById("start").addEventListener("click",drawDot);
+document.getElementById("dvd").addEventListener("click",dvd);
+document.getElementById("stop").addEventListener("click",stop);
